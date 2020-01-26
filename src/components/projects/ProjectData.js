408f840c6bcb1076a02aaa8cbd7e3a4a74f9f051
Lizzie <lchan217@gmail.com>
2020-01-25 18:39:14 -0700

    add star wars data to projectdata

---
+++ src/components/projects/ProjectData.js
@@ -0,0 +1,8 @@
+export const ProjectData = {
+  Image: "Image",
+  Title: "Star Wars API",
+  Text:
+    "Web app that fetches from swapi.co and displays Star Wars Movies and their characters upon click",
+  Link: "Go to this SPA",
+  Footer: "Languages: CSS and JavaScript"
+};
