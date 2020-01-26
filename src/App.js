b0fa828942675a7d932d2c6b3f4b89e466e9965f
Lizzie <lchan217@gmail.com>
2020-01-25 19:44:42 -0700

    add container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import "./App.css";
 import ProjectCards from "./components/projects/ProjectCards";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Mini Projects</h1>
-      <p>
-        This is a compilation of mini-projects from take-home tech interview
-        challenges and {""}
-        <a
-          href='https://www.frontendmentor.io/challenges'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          frontend mentor challenges
-        </a>
-        . Enjoy!
-      </p>
-      <ProjectCards />
+      <Container>
+        <h1>Mini Projects</h1>
+        <p>
+          This is a compilation of mini-projects from take-home tech interview
+          challenges and {""}
+          <a
+            href='https://www.frontendmentor.io/challenges'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            frontend mentor challenges
+          </a>
+          . Enjoy!
+        </p>
+        <ProjectCards />
+      </Container>
     </div>
   );
 }
