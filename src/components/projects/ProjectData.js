cccd1768e5cbebe183a741e4b568c903eabdfbe9
Lizzie <lchan217@gmail.com>
2020-01-25 19:42:10 -0700

    add 2 more projects to data

--- src/components/projects/ProjectData.js
+++ src/components/projects/ProjectData.js
@@ -2,9 +2,26 @@ const project1 = {
   image: "Image",
   title: "Star Wars API",
   text:
-    "Web app that fetches from swapi.co and displays Star Wars Movies and their characters upon click",
+    "Web app that fetches from swapi.co and displays Star Wars movies and their characters upon click",
   link: "Go to this SPA",
   footer: "Technologies and Frameworks: CSS and JavaScript"
 };
 
-export const ProjectData = [project1];
+const project2 = {
+  image: "Image",
+  title: "Weather App",
+  text:
+    "Web app that fetches Munich weather data and displays daily average on cards and daily weather in 3 hour increments in a bar graph",
+  link: "Go to this SPA",
+  footer: "Technologies and Frameworks: React, CSS, JavaScript, Semantic UI"
+};
+
+const fem1 = {
+  image: "Image",
+  title: "Coming Soon Landy Page",
+  text: "Webpage that shows a landing page for a future company",
+  link: "Go to this SPA",
+  footer: "Technologies and Frameworks: CSS and JavaScript"
+};
+
+export const ProjectData = [project1, project2, fem1];
