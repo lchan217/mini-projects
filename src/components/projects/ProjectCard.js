18dbb26548259f623823091a2290599db50a54d4
Lizzie <lchan217@gmail.com>
2020-01-25 18:55:19 -0700

    display data on card

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
-const ProjectCard = () => {
+const ProjectCard = ({ project: { image, title, text, link, footer } }) => {
   return (
     <div>
-      PrjectCardComponent
       <Card style={{ width: "18rem" }}>
-        <h1> Image: thumbnail of SPA </h1>
+        <h1> {image} </h1>
         {/* <Card.Img variant='top' src='holder.js/100px180' /> */}
         <Card.Body>
-          <Card.Title>Title: Star Wars API</Card.Title>
-          <Card.Text>Text: Summary what app does</Card.Text>
-          <Button variant='primary'>Link: Link to SPA</Button>
+          <Card.Title> {title}</Card.Title>
+          <Card.Text> {text}</Card.Text>
+          <Button variant='primary'> {link}</Button>
         </Card.Body>
-        <Card.Footer>Footer: Languages- CSS and JS</Card.Footer>
+        <Card.Footer>{footer}</Card.Footer>
       </Card>
     </div>
   );
