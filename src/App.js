82a8432f9663cc28007dd65ddd7ceef476687dff
Lizzie <lchan217@gmail.com>
2020-01-26 10:39:57 -0700

    remove routes - leave react-router-dom for now, may delete later

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Intro from "./components/Intro";
-import StarWars from "./components/starWars/StarWars";
 
 function App() {
   return (
     <div className='App'>
       <Router>
         <Route exact path='/' component={Intro} />
-        <Route exact path='/star-wars' component={StarWars} />
-        {/* <Route exact path='/weather-app' component={WeatherApp} />
-        <Route exact path='/coming-soon' component={ComingSoon} /> */}
       </Router>
     </div>
   );
