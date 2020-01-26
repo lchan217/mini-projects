55f4284e7e2216da8a40045677fa6cdd83086f69
Lizzie <lchan217@gmail.com>
2020-01-25 18:29:49 -0700

    create ProjectCard skeleton and move card data  from app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Card, Button } from "react-bootstrap";
+import ProjectCard from "./components/ProjectCard";
 
 function App() {
   return (
@@ -18,18 +18,7 @@ function App() {
         </a>
         . Enjoy!
       </p>
-      <div>
-        <Card style={{ width: "18rem" }}>
-          <h1> Image: thumbnail of SPA </h1>
-          {/* <Card.Img variant='top' src='holder.js/100px180' /> */}
-          <Card.Body>
-            <Card.Title>Title: Star Wars API</Card.Title>
-            <Card.Text>Text: Summary what app does</Card.Text>
-            <Button variant='primary'>Link: Link to SPA</Button>
-          </Card.Body>
-          <Card.Footer>Footer: Languages- CSS and JS</Card.Footer>
-        </Card>
-      </div>
+      <ProjectCard />
     </div>
   );
 }
